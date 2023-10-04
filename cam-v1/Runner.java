33e5e198d7869953c1172d09d0d2d6d3e1a75e66
Mark L <markjl24@ausdk12.org>
2023-10-04 12:25:37 -0700

    Hehehehehahahahahah

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,5 +1,8 @@
 public class Runner {
     public static void main(String[] args){
-        new VirtualPet();
+        VirtualPet v1 = new VirtualPet();
+        v1.feed();
+        v1.exercise();
+        v1.sleep();
     }
 }
