00f4b58d993191a5fb2080f161f3c7c73f450517
Mark L <markjl24@ausdk12.org>
2023-10-11 11:31:48 -0700

    fff

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,8 +1,44 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Runner {
-    public static void main(String[] args){
+    public Runner(){
         VirtualPet v1 = new VirtualPet();
-        v1.feed();
         v1.exercise();
+        sleep(1000);
+        v1.feed();
+        sleep(1000);
         v1.sleep();
+        String ans = getAnswer("How are you?");
+        if(ans.equalsIgnoreCase("Good")){
+            
+        }
+    }
+
+    public String getAnswer(String q){
+        String s = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    q,
+                    "Get a Response",
+                    JOptionPane.PLAIN_MESSAGE
+
+
+    
+);
+return s;
+    }
+    
+
+    public static void main(String[] args){
+        new Runner();
+    }
+
+    public void sleep(int milliseconds){
+        try {
+            Thread.sleep(milliseconds); //milliseconds
+        } catch(Exception e){
+        
+        }
+
     }
 }
